cabb85095d7450922324c3ea96982f4ebceeb75f
Dawei Chen <dawei.chen@anu.edu.au>
2021-12-23 14:58:28 +1100

    optionally specifying a degree identifier

--- basket/extract.sh
+++ basket/extract.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 DB_NAME="fais"
-QUERY="SELECT s.username AS uid, u.unitcode AS course FROM student s, unit u, enrolment e WHERE e.studentid = s.id AND e.unitid = u.unitid;"
+
+if [ $# -eq 1 ]; then
+    DEGREE=$1
+    QUERY="SELECT s.username AS uid, u.unitcode AS course FROM student s, unit u, enrolment e WHERE e.studentid = s.id AND e.unitid = u.unitid AND s.degree='$DEGREE';"
+else
+    QUERY="SELECT s.username AS uid, u.unitcode AS course FROM student s, unit u, enrolment e WHERE e.studentid = s.id AND e.unitid = u.unitid;"
+fi
+
+# echo "$QUERY"
 
 sudo -u postgres psql $DB_NAME --csv -c "$QUERY"
