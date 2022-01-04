24da5831afad59071d1e3b02e2c11a8690abc2a1
Graham Williams <Graham.Williams@togaware.com>
2022-01-05 09:21:26 +1100

    Lint updates.

--- basket/baskets.R
+++ basket/baskets.R
@@ -1,23 +1,35 @@
 #!/usr/bin/env Rscript
-import::here(apriori, inspect, sort, head, .from=arules)
-import::here(arg_parser, add_argument, parse_args, .from=argparser)
+
+import::here(apriori, inspect, sort, head, .from = arules)
+import::here(arg_parser, add_argument, parse_args, .from = argparser)
 
 
-parser <- arg_parser("Simple basket analysis");
-parser <- add_argument(parser, "--csv", help="CSV data filename", default=NULL, type="character", short="-f");
-parser <- add_argument(parser, "--support", help="The support of Apriori algorithm", default=0.1, type="double", short="-s");
+parser <- arg_parser("Simple basket analysis")
+parser <- add_argument(parser, "datafile",
+                       help = "CSV data filename",
+                       default = NULL,
+                       type = "character")
+parser <- add_argument(parser, "--support",
+                       help = "The support of Apriori algorithm",
+                       default = 0.1,
+                       type = "double",
+                       short = "-s")
 argv <- parse_args(parser);
 
-if (is.na(argv$csv)) {
+if (is.na(argv$datafile)) {
     print(parser);
-    stop("No CSV data file provided\n", call.=FALSE);
+    stop("No CSV data file provided\n", call. = FALSE)
 }
 
-dataset <- read.csv(argv$csv,
-                    na.strings=c(".", "NA", "", "?"),
-		            strip.white=TRUE, encoding="UTF-8");
-transactions <- as(split(dataset$course, dataset$uid), "transactions");
+dataset <- read.csv(argv$datafile,
+                    na.strings = c(".", "NA", "", "?"),
+                    strip.white = TRUE, encoding = "UTF-8")
+transactions <- as(split(dataset$course, dataset$uid), "transactions")
 
-rules <- apriori(transactions, parameter = list(support=argv$support, confidence=0.1, minlen=2));
-inspect(head(rules, n=5, by="support", decreasing=TRUE));
+rules <- apriori(transactions,
+                 parameter = list(support = argv$support,
+                 confidence = 0.1,
+                 minlen = 2))
+inspect(head(rules, n = 5, by = "support", decreasing = TRUE))
+
 # inspect(sort(rules, by="support"));  # print all rules
