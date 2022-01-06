7e010bb33baf095c43e93598a89fa5c1ba15b0db
Dawei Chen <dawei.chen@anu.edu.au>
2022-01-06 17:45:49 +1100

    output frequent patterns rather than arules #8

--- basket/baskets.R
+++ basket/baskets.R
@@ -1,11 +1,9 @@
 #!/usr/bin/env Rscript
 
-suppressPackageStartupMessages({
-  library(dplyr)
-  library(arules)
-  library(argparser)
-  library(glue)
-})
+# library(tidyverse, quietly = TRUE)
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(arules))
+suppressPackageStartupMessages(library(argparser))
 
 
 parser <- arg_parser("Simple basket analysis")
@@ -34,25 +32,25 @@ dataset <- read.csv(argv$datafile,
                     strip.white = TRUE, encoding = "UTF-8")
 
 if (!(argv$id %in% colnames(dataset))) {
-  msg = glue("A basket identifier '{argv$id}' ",
-             "was not found amongst the columns: ",
-             "{paste(names(dataset), collapse=', ')}.\n",
-             "      Use '--id=' to identify the identifier.")
-  stop(msg, call. = FALSE)
+    print(parser)
+    stop(sprintf(fmt="The identification variable \"%s\" does not exist in the dataset\n", argv$id), call. = FALSE)
 }
 
-dataset %>%
-  rename(id=argv$id) %>%
-  select(id, everything()) ->
-df
+dataset %>% 
+    rename(id=argv$id) %>% 
+    select(id, everything()) -> df
 
-transactions <- as(split(df[, 2], df[, 1]), "transactions")
-
-rules <- apriori(transactions,
-                 parameter = list(support = argv$support,
-                                  confidence = 0.1,
-                                  minlen = 2),
-                 control = list(verbose = FALSE))
-inspect(head(rules, n = 5, by = "support", decreasing = TRUE))
-
-# inspect(sort(rules, by="support"));  # print all rules
+as(split(df[, 2], df[, 1]), "transactions") %>%
+    apriori(parameter = list(
+                support = argv$support,
+                confidence = argv$confidence,
+                minlen = 2,
+                target = "frequent itemsets"),
+            control = list(verbose = FALSE)
+    ) %>%
+    as("data.frame") %>%
+    rename(
+        pattern = items,
+        freq = count
+    ) %>%
+    select(pattern, freq, support)
