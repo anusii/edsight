6d3d8ee263b680b1c0449bfe0a3702dd4a38e3e0
Graham Williams <Graham.Williams@togaware.com>
2022-02-16 09:33:40 +1100

    --input not required. Process command line in one place.

--- basket/itemsets.R
+++ basket/itemsets.R
@@ -13,12 +13,6 @@ setwd(get_cmd_cwd())
 
 parser <- OptionParser()
 parser <- add_option(parser,
-                     c("-i", "--input"),
-                     action = "store",
-                     default = "stdin",
-                     help = "Input CSV data file [default: %default]",
-                     type = "character")
-parser <- add_option(parser,
                      c("-o", "--output"),
                      action = "store",
                      default = "stdout",
@@ -40,24 +34,30 @@ argv <- parse_args(parser, positional_arguments=TRUE)
 
 # Handle positional argument - input filename
 
-if (length(argv$args)) {
-  argv$options$input = argv$args[1]
-}
+input <- ifelse(length(argv$args),
+                argv$args[1],
+                "stdin")
 
-fext = ifelse(argv$options$output == "stdout",
-              "stdout",
-              file_ext(argv$options$output))
+output <- argv$options$output
+
+fext <- ifelse(output == "stdout",
+               "stdout",
+               file_ext(output))
+
+ident <- argv$options$id
+
+threshold <- argv$options$support
 
 if (! fext %in% c("stdout", "csv", "rds"))
   stop(glue("file extension expected as either 'csv' or 'rds' ",
             "found '{fext}'."))
 
-dataset <- read.csv(file(argv$options$input),  # file() is necessary to read from stdin
+dataset <- read.csv(file(input),  # file() is necessary to read from stdin
                     na.strings = c(".", "NA", "", "?"),
                     strip.white = TRUE, encoding = "UTF-8")
 
-if (!(argv$options$id %in% colnames(dataset))) {
-    msg = glue("A basket identifier '{argv$options$id}' ",
+if (!(ident %in% colnames(dataset))) {
+    msg = glue("A basket identifier '{ident}' ",
                "was not found amongst the columns: ",
                "{paste(names(dataset), collapse=', ')}.\n",
                "      Use '--id=<column>' to specify the identifier.")
@@ -65,13 +65,13 @@ if (!(argv$options$id %in% colnames(dataset))) {
 }
 
 dataset %>% 
-    rename(id=argv$options$id) %>% 
+    rename(id=!!ident) %>% 
     select(id, everything()) -> 
 df
 
 as(split(df[, 2], df[, 1]), "transactions") %>%
     apriori(parameter = list(
-                support = argv$options$support,
+                support = threshold,
                 minlen = 2,
                 target = "frequent itemsets"),
             control = list(verbose = FALSE)
@@ -80,7 +80,7 @@ as(split(df[, 2], df[, 1]), "transactions") %>%
 itemsets
 
 if (fext == "rds") {
-  saveRDS(itemsets, argv$options$output)
+  saveRDS(itemsets, output)
 } else {
   itemsets %>%
     as("data.frame") %>%
@@ -88,7 +88,7 @@ if (fext == "rds") {
     select(pattern, freq, support) ->
   df
   if (fext == "csv")
-    write.csv(df, argv$options$output, row.names=FALSE)
+    write.csv(df, output, row.names=FALSE)
   else
     write.csv(df, "", row.names=FALSE)
 }
