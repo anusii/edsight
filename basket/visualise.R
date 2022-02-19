ef8b988819d40509eae4c475fc08c820b2d09b64
Graham Williams <Graham.Williams@togaware.com>
2022-02-19 17:08:58 +1100

    Simplify the interface - don't build model, just visualise.

--- basket/visualise.R
+++ basket/visualise.R
@@ -7,70 +7,30 @@ suppressPackageStartupMessages({
     library(glue)
     library(mlhub)
     library(arulesViz)
-    library(fs)
+    library(mlhub)
 })
 
 setwd(get_cmd_cwd())
 
 parser <- OptionParser()
-parser <- add_option(parser,
-                     c("-f", "--datafile"),
-                     action = "store",
-                     default = "stdin",
-                     help = "CSV data file [default: standard input]",
-                     type = "character")
-parser <- add_option(parser,
-                     "--id",
-                     action = "store",
-                     default = "id",
-                     help = "The basket identification variable/column [default: %default]",
-                     type = "character")
-parser <- add_option(parser,
-                     c("-s", "--support"),
-                     action = "store",
-                     default = 0.1,
-                     help = "The support for the Apriori algorithm [default: %default]",
-                     type = "double")
-parser <- add_option(parser,
-                     c("-c", "--confidence"),
-                     action = "store",
-                     default = 0.1,
-                     help = "The confidence for the Apriori algorithm [default: %default]",
-                     type = "double")
-argv <- parse_args(parser)
+argv <- parse_args(parser, positional_arguments=TRUE)
 
-dataset <- read.csv(file(argv$datafile),  # file() is necessary to read from stdin
-                    na.strings = c(".", "NA", "", "?"),
-                    strip.white = TRUE, encoding = "UTF-8")
+if (length(argv$args) != 1)
+  stop("Exactly on argument is required: rds filename containin model")
 
-if (!(argv$id %in% colnames(dataset))) {
-    msg = glue("A basket identifier '{argv$id}' ",
-               "was not found amongst the columns: ",
-               "{paste(names(dataset), collapse=', ')}.\n",
-               "      Use '--id=<column>' to specify the identifier.")
-    stop(msg, call. = FALSE)
-}
+input  <- argv$args[1]
 
-dataset %>% 
-    rename(id=argv$id) %>% 
-    select(id, everything()) -> 
-df
-
-as(split(df[, 2], df[, 1]), "transactions") %>%
-    apriori(parameter = list(
-                support = argv$support,
-                confidence = argv$confidence,
-                minlen = 2,
-                target = "rules"),
-            control = list(verbose = FALSE)
-    ) %>%
-    sort(by = "support") %>%
-    plot(method = "graph",
-         shading = "support",
-         measure = "confidence", 
-         limit = 10)
+readRDS(input) %>%
+  sort(by = "support") %>%
+  plot(method = "graph",
+       shading = "support",
+       measure = "confidence",
+       engine="ggplot2",
+       limit = 10)
 
 # This is a hack to show the visualisation in non-interactive mode where
 # the plot is automatically saved to file `Rplots.pdf`.
 # However, a more "natural" approach would be preferred.
-file_show(path(getwd(), "Rplots.pdf"))
+
+mlpreview("Rplots.pdf")
+
